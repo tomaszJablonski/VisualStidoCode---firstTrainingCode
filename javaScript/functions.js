e9fc1cbacd36dc1158d0e4c23f13e0c896d27155
https://github.com/tomaszJablonski <tjablonski1990@gmail.com>
2024-06-24 20:52:48 +0200

    functions

---
+++ javaScript/functions.js
@@ -0,0 +1,13 @@
+function functionName(param1, param2){
+    //statements
+}
+
+function add(a, b){
+    // console.log(a+b)
+    return a + b 
+}
+
+let result = add(10,5)
+let result2 = add(2,8)
+
+console.log(result,result2)
