968bd7e994ffa07ba36c79e302dbdd692083d3f7
https://github.com/tomaszJablonski <tjablonski1990@gmail.com>
2024-06-24 16:25:35 +0200

    ifElseIf

---
+++ javaScript/ifElseIf.js
@@ -0,0 +1,15 @@
+/* 
+if statement
+if..else statement
+if..esle.if statement
+*/
+
+let age = 18;
+
+if(age>18){
+    console.log('Age is right')
+} else if (age == 18) {
+    console.log('Nice, congratulations')
+}else {
+    console.log('You are too young, sorry')
+}
