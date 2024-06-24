d90e4fa9c52a9975fcf34b40e50467cbe1182263
https://github.com/tomaszJablonski <tjablonski1990@gmail.com>
2024-06-24 16:35:23 +0200

    switch

---
+++ javaScript/switchCase.js
@@ -0,0 +1,24 @@
+let a = 4;
+
+switch(a){
+    case 1: 
+        console.log('Number 1');
+        break;
+    case 2: 
+        console.log('Number 2');
+        break;
+    case 3: 
+        console.log('Number 3');
+        break;
+    case 4: 
+        console.log('Number 4');
+        break;
+    case 5: 
+        console.log('Number 5');
+        break;
+    case 6: 
+        console.log('Number 6');
+        break;
+    default: 
+        console.log('There is no number.Enter number between 1-6')    
+} 
